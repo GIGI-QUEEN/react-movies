eec206345b766961e16c591d50782f1af193c21e
ARI <nikitos52@gmail.com>
2022-07-12 19:00:23 +0300

    https added

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -12,20 +12,28 @@ class Main extends React.Component {
   };
 
   componentDidMount() {
-    fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&s=harry%20potter`)
+    fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=harry%20potter`)
       .then((response) => response.json())
-      .then((data) => this.setState({ movies: data.Search, loading: false }));
+      .then((data) => this.setState({ movies: data.Search, loading: false }))
+      .catch((err) => {
+        console.error(err);
+        this.setState({ loading: false });
+      });
   }
 
   searchMovies = (title, type = "all") => {
     this.setState({ loading: true });
     fetch(
-      `http://www.omdbapi.com/?apikey=${API_KEY}&s=${title}${
+      `https://www.omdbapi.com/?apikey=${API_KEY}&s=${title}${
         type !== "all" ? `&type=${type}` : ""
       }`
     )
       .then((response) => response.json())
-      .then((data) => this.setState({ movies: data.Search, loading: false }));
+      .then((data) => this.setState({ movies: data.Search, loading: false }))
+      .catch((err) => {
+        console.error(err);
+        this.setState({ loading: false });
+      });
   };
 
   render() {
